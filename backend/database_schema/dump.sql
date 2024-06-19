85e7dbb670d7c249c0ace3657ded9395ee5422d2
eexvuu2 <storageuser54@gmail.com>
2024-06-19 21:58:39 +0700

    edit dump.sql

--- backend/database_schema/dump.sql
+++ backend/database_schema/dump.sql
@@ -1,20 +1,20 @@
 
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+CREATE TABLE tasks (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(2550) NOT NULL,
+  status int(11) NOT NULL,
+  user_id int(11) DEFAULT NULL,
+  createdAt datetime DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY user_id (user_id),
+  CONSTRAINT tasks_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id)
 ) 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `createdAt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  createdAt datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY email (email)
 ) 
