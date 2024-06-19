32761634bb84801582d0243d469c9ef2c63d7d67
eexvuu2 <storageuser54@gmail.com>
2024-06-19 22:02:45 +0700

    edit dump.sql

--- backend/database_schema/dump.sql
+++ backend/database_schema/dump.sql
@@ -1,3 +1,12 @@
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  createdAt datetime NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY email (email)
+);
 
 CREATE TABLE tasks (
   id int(11) NOT NULL AUTO_INCREMENT,
@@ -8,13 +17,4 @@ CREATE TABLE tasks (
   PRIMARY KEY (id),
   KEY user_id (user_id),
   CONSTRAINT tasks_ibfk_1 FOREIGN KEY (user_id) REFERENCES users (id)
-) 
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  createdAt datetime NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY email (email)
-) 
+);
